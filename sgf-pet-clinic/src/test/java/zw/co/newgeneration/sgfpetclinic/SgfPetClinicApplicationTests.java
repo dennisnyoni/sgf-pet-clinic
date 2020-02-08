a625d4ce3fd61009d2a3e337115651f1771bb215
dennisnyoni <dennisnyoni@rocketmail.com>
2020-02-08 14:34:46 +0200

    pet clinic
    pet clinic

---
+++ sgf-pet-clinic/src/test/java/zw/co/newgeneration/sgfpetclinic/SgfPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package zw.co.newgeneration.sgfpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgfPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
